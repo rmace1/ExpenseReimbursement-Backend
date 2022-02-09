accfd13c057af0c46090e44a69f0549043e94169
rmace1 <93550062+rmace1@users.noreply.github.com>
2022-02-09 01:00:33 -0500

    Delete target directory

--- target/classes/frontend/global/global.js
+++
@@ -1,9 +0,0 @@
-var domain = "http://localhost:9000";
-
-async function logout(){
-    let response = await fetch(domain + "/api/session", {
-        method: "DELETE"
-    })
-
-    window.location.href = domain;
-}
